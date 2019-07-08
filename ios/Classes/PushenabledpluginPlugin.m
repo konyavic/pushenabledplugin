e35804b9f4a44501c9294a88f9da4f1b6f3cf6e0
Chenyi Lee <chenyi@repro.io>
2019-07-08 12:20:57 +0900

    add ios implementation

--- ios/Classes/PushenabledpluginPlugin.m
+++ ios/Classes/PushenabledpluginPlugin.m
@@ -1,17 +1,21 @@
 #import "PushenabledpluginPlugin.h"
+#import <UserNotifications/UserNotifications.h>
 
 @implementation PushenabledpluginPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"pushenabledplugin"
+      methodChannelWithName:@"com.konyavic.pushenabledplugin/pushenabledplugin"
             binaryMessenger:[registrar messenger]];
   PushenabledpluginPlugin* instance = [[PushenabledpluginPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"areNotificationsEnabled" isEqualToString:call.method]) {
+      [UNUserNotificationCenter.currentNotificationCenter getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+          BOOL enabled = (settings.authorizationStatus == UNAuthorizationStatusAuthorized);
+          result([NSNumber numberWithBool:enabled]);
+      }];
   } else {
     result(FlutterMethodNotImplemented);
   }
